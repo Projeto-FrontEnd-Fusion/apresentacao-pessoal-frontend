18851a951f17912710e56128683a3f259675a283
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-17 05:59:17 -0300

    CHORE : ajust and delete default files of vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Seja bem vindo! :)</h1>
+        <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
+      </main>
+    </>
+  );
+}
+
+export default App
