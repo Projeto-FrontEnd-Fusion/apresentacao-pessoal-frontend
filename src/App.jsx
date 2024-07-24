ba32eb6d1f36d2b9edc8fee0a106988ebf6450b3
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 17:36:44 -0300

    FEAT: importing components and adjusting code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@
 import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
+import SocialLinks from './components/SocialLinks';
 import './App.css'
 
-function App() {
+const App = () => {
 
   return (
     <div className="App max-w-4xl mx-auto">
       <Header />
       <About />
       <Skills />
+      <Projects />
+      <SocialLinks />
 
     </div>
   );
