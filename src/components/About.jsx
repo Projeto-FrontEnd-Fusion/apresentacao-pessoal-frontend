4ddb85f1d92071d4fe06d5c398fe08cfea7f88e7
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 00:37:24 -0300

    feat: add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section classname="p-4">
+            <h2 classname="text-3x1 mb-2">Sobre Mim</h2>
+            <p>Sou desenvolvedor backend com experiência em C#, Node.js e .NET. Sou apaixonado por construir aplicações escaláveis e eficientes.</p>
+        </section>
+    )
+}
