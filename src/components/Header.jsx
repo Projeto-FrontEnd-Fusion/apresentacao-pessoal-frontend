5a7133bf54ed0d1b180d51cbeefa6a3a2a026f1b
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 17:27:49 -0300

    :lipstick: FEAT: adding photo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white p-4 text-center">
+    <header className="header bg-blue-600 text-white p-4 text-center">
+      <img src="/src/assets/foto-fundo-preto-leo.png" alt="Profile" className="profile-img" />
       <h1 className="text-4xl">Leonardo Matheus</h1>
       <p className="text-xl">Desenvolvedor Backend Jr</p>
     </header>
