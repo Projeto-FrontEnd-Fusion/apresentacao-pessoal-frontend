449d082dc769301e842f5f49e5e87bfab770eb74
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 01:21:05 -0300

    refactor: Corrigindo o codigo apos biblioteca TailwindCSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 
 const About = () => {
     return (
-        <section classname="p-4">
-            <h2 classname="text-3x1 mb-2">Sobre Mim</h2>
+        <section className="p-4">
+            <h2 className="text-3x1 mb-2">Sobre Mim</h2>
             <p>Sou desenvolvedor backend com experiência em C#, Node.js e .NET. Sou apaixonado por construir aplicações escaláveis e eficientes.</p>
         </section>
     )
 }
+
+export default About;
