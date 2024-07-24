e92cf55cbb3897b54e55b0b4b622fe5879c576da
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 02:26:00 -0300

    feat: Add Skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Skills = () => {
+    <section className="p-4 bg-gray-100">
+        <h2 className="text-3-1 mb-2">Skills</h2>
+        <ul className="list-disc list-inside">
+            <li>C#</li>
+            <li></li>
+        </ul>
+    </section>
+}
+
+export default Skills;
