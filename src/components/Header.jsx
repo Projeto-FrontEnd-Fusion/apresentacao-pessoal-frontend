7008912075cb5b00d1c094d191a8a7a5b6834d98
LeoST25 <matheusleonardo224@gmail.com>
2024-07-23 23:17:35 -0300

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 text-center">
+      <h1 className="text-4xl">Leonardo Matheus</h1>
+      <p className="text-xl">Desenvolvedor Backend Jr</p>
+    </header>
+  );
+};
+
+export default Header;
