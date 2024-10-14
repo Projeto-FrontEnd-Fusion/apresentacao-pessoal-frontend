5b5b1eca9d36ce97c47f4336fdf72433ca9db6e0
Rafael Molinari <progedesenvolvimento@outlook.com.br>
2024-10-14 20:41:54 -0300

    Commit pré finalização

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <nav
+      className="navegar"
+    >
+      <div className="container">
+        <div className="navbarResponsive">
+          <ul className="row">
+            <li className="col-md-4">
+              <a className="menu_1" href="#projetos">
+                Portfolio
+              </a>
+            </li>
+            <li className="col-md-4">
+              <a className="menu_1" href="#skills">
+                Habilidades
+              </a>
+            </li>
+            <li className="col-md-4">
+              <a className="menu_1" href="#contact">
+                Contato
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/pages/Contato/Contato.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contato(){
+    return(
+        <div className="container">
+            <div className="col-sm-4">
+                <h4>Teste</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Contato
