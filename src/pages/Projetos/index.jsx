09aa055cd7bf8fe01ddc3da42af588d9276f5bb0
João Batista Rodrigues Ribeiro <87319898+Jbrr2021@users.noreply.github.com>
2024-10-13 22:06:50 -0300

    Corrige caminhos de imagens no componente Projetos

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -7,20 +7,20 @@ function Projetos() {
         {
             id: 1,
             title: 'Projeto 1',
-            imageUrl: '/PP.png', // Substitua pelo caminho da imagem
-            repoUrl: 'https://lnkd.in/dXw8N8Ki', // Substitua pelo link do repositório
-            liveUrl: 'https://seu-site.com/projeto1', // Substitua pelo link da página web
+            imageUrl: '/PP.png', 
+            repoUrl: 'https://lnkd.in/dXw8N8Ki', 
+            liveUrl: 'https://seu-site.com/projeto1', 
             technologies: ['React', 'Tailwind CSS', 'Vite'],
         },
         {
             id: 2,
             title: 'Relógio Digital Dinâmico',
-            imageUrl: '/relogio.png', // Substitua pelo caminho da imagem
-            repoUrl: 'https://github.com/seu-usuario/projeto2', // Substitua pelo link do repositório
-            liveUrl: 'https://lnkd.in/dqjzw5Zv', // Substitua pelo link da página web
+            imageUrl: '/relogio.png', 
+            repoUrl: 'https://github.com/seu-usuario/projeto2', 
+            liveUrl: 'https://lnkd.in/dqjzw5Zv', 
             technologies: ['JavaScript', 'HTML', 'CSS'],
         },
-        // Adicione mais projetos conforme necessário
+        
     ];
 
     return (
